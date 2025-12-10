5f4e696233d37f6698b8efdf6c6477612db0d229
David Pochet <david.pochet@gmail.com>
2025-12-10 19:07:08 +0100

    (Bad) solution for day 10 step 2

--- src/aoc2025/day10.py
+++ src/aoc2025/day10.py
@@ -2,7 +2,7 @@ import functools
 import operator
 import re
 from dataclasses import dataclass
-from itertools import combinations
+from itertools import combinations, combinations_with_replacement
 from pathlib import Path
 
 from .puzzle import AOCPuzzle
@@ -19,7 +19,8 @@ MACHINE_PATTERN = re.compile("\\[(.+)\\](.+)\\{(.+)\\}")
 class Machine:
     target: int
     buttons: set[int]
-    joltages: set[int]
+    counter_buttons: list[list[int]]
+    joltages: list[int]
 
 
 class D10Puzzle(AOCPuzzle):
@@ -43,15 +44,24 @@ class D10Puzzle(AOCPuzzle):
 
             # Parse buttons
             buttons: set[int] = set()
+            counter_buttons: list[list[int]] = []
             for button_str in (")" + m.group(2) + "(").split(") ("):
                 if button_str:
                     button = 0
+                    counter_button: list[int] = []
                     for bit in button_str.split(","):
                         button += 2 ** int(bit)
+                        counter_button.append(int(bit))
                     buttons.add(button)
+                    counter_buttons.append(counter_button)
+
+            # Parse joltages
+            joltages: list[int] = []
+            for joltage_str in m.group(3).split(","):
+                joltages.append(int(joltage_str))
 
             # Add machine
-            machine = Machine(target, buttons, set())
+            machine = Machine(target, buttons, counter_buttons, joltages)
             self._logger.info(f"parsed machine on line {index}: {machine}")
             self._machines.append(machine)
 
@@ -81,4 +91,24 @@ class D10Step1Puzzle(D10Puzzle):
 
 class D10Step2Puzzle(D10Puzzle):
     def solve(self, some_arg: int | str | None = None) -> int:
-        return 0
+        # Iterate on machines
+        total = 0
+        for machine in self._machines:
+            count_candidate = 1
+            found_candidate = 0
+            while found_candidate == 0:
+                # Iterate on all combinations
+                for buttons_combi in combinations_with_replacement(machine.counter_buttons, count_candidate):
+                    # Compute counters from button pushes
+                    counters = [0] * len(machine.joltages)
+                    for button in buttons_combi:
+                        for pos in button:
+                            counters[pos] = counters[pos] + 1
+                    # If counters match joltages, we're good
+                    if counters == machine.joltages:
+                        found_candidate = count_candidate
+                        break
+                else:
+                    count_candidate += 1
+            total += found_candidate
+        return total
---
+++ src/tests/test_day10.py
@@ -0,0 +1,17 @@
+from aoc2025.day10 import D10Step1Puzzle, D10Step2Puzzle
+
+from .base import AOCPuzzleTester
+
+
+class TestD10(AOCPuzzleTester):
+    def test_step1_sample(self):
+        self.check_solution(D10Step1Puzzle, "d10.sample.txt", 7)
+
+    def test_step1_input(self):
+        self.check_solution(D10Step1Puzzle, "d10.input.txt", 409)
+
+    def test_step2_sample(self):
+        self.check_solution(D10Step2Puzzle, "d10.sample.txt", 33)
+
+    def test_step2_input(self):
+        self.check_solution(D10Step2Puzzle, "d10.input.txt", 0)
